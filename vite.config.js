b1090b969fc4a1d8777d9d83e4fcbad9a3018588
Alexei Karyavin <akaryavin@dataplus.ru>
2022-12-07 01:17:34 +0300

    pages test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: "/arjs-project-test",
   server: { https: true },
   plugins: [basicSsl()],
 });
