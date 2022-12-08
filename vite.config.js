2e22ddb12d70df1857d1e9aba20060120f9f128b
Alexei Karyavin <akaryavin@dataplus.ru>
2022-12-08 03:43:47 +0300

    3В

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "" : "/arjs-project-test/",
-  server: { https: true },
-  plugins: [basicSsl()],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  console.log("process.env", process.env.NODE_ENV);
+  console.log("import", import.meta.env);
+
+  return defineConfig({
+    base: process.env.NODE_ENV === "development" ? "" : "/arjs-project-test/",
+    server: { https: true },
+    plugins: [basicSsl()],
+  });
+};
